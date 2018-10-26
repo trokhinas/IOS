1451e32912140649b828f81992a96b96dbe2a1ff
Anton <anton@mail.com>
2018-10-26 15:02:39 +0300

    developing protocol theme

---
+++ Concentration/DefaultTheme.swift
@@ -0,0 +1,9 @@
+//
+//  DefaultTheme.swift
+//  Concentration
+//
+//  Created by xcode on 26.10.2018.
+//  Copyright © 2018 VSU. All rights reserved.
+//
+
+import Foundation
---
+++ Concentration/ThemeClass.swift
@@ -0,0 +1,9 @@
+//
+//  ThemeClass.swift
+//  Concentration
+//
+//  Created by xcode on 26.10.2018.
+//  Copyright © 2018 VSU. All rights reserved.
+//
+
+import Foundation
